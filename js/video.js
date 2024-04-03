cfc86ff19fb820818ad30134d02d75b5ffe3b787
fahmidss <fahmida@umich.edu>
2024-04-03 15:50:26 -0400

    done?

--- js/video.js
+++ js/video.js
@@ -3,7 +3,7 @@ var video;
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
   	video = document.querySelector("#player1");
-  	video.autoplau = false;
+  	video.autoplay = false;
   	video.loop = false;
   	console.log("auto play is set to " + video.autoplay);
   	console.log("loop is set to " + video.loop);
@@ -66,5 +66,26 @@ document.querySelector("#mute").addEventListener("click", function() {
 		console.log("Video is muted");
 		document.getElementById("mute"). innerHTML = "Unmute";
 	}
+}
+);
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector("#volume").innerHTML = 100 * (video.volume) + "%";
+	console.log("Volume is " + (100 * video.volume) + "%");
+}
+);
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Utilizing oldSchool class")
+	video = document.querySelector("#player1");
+	video.classList.replace("video", "oldSchool");
+}
+);
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Removing oldSchool class")
+	video = document.querySelector("#player1");
+	video.classList.remove("video", "oldSchool");
 }
 );
